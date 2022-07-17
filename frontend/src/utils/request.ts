aac5be5f60f76d6e17cd46350d6d734095845f2c
Maiara Santos <maiarasantos34@yahoo.com>
2022-07-17 20:41:05 -0300

    Mensagem

--- frontend/src/utils/request.ts
+++ frontend/src/utils/request.ts
@@ -1,7 +1,3 @@
+
 export const BASE_URL = import.meta.env.VITE_BACKEND_URL ?? "http://localhost:8080";
 
-export const coinFormat = {
-    minimumFractionDigits: 2,
-    style: 'currency',
-    currency: 'BRL',
-  };
